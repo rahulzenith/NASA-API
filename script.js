6d68c31e744910d03c5af589c416e14d51017f6d
Rahul uprety <rahul.uprety0@gmail.com>
2019-07-31 00:54:19 +0530

    NASA API

---
+++ script.js
@@ -0,0 +1,96 @@
+var button = $('#append-button');
+
+console.log(button);
+
+var message = "message";
+
+button.click(function () {
+
+  $('#hi').remove();
+
+});
+
+
+var dogImageButton = $('#get-dog-image');
+
+function updateImageTag(imageLink) {
+  $('#dog-random-image').attr('src', imageLink);
+}
+
+function getImage() {
+  var breed = $('#dog-breed').val();
+  $.ajax({
+    type: 'get',
+    // random image
+    // url: 'https://dog.ceo/api/breeds/image/random',
+
+    //for breed image 
+    url: 'https://dog.ceo/api/breed/' + breed + '/images/random',
+
+    success: function (responseData) {
+      console.log('response Data: ', responseData);
+      updateImageTag(responseData.message);
+    },
+    error: function () {
+      console.log('some error has occured');
+    }
+
+  });
+}
+
+
+dogImageButton.click(function () {
+  getImage();
+});
+
+
+// this method creates an image tag and appends it to the image-grid element
+function createImage(imageLink) {
+
+  // create a tag in memory
+  let newImg = $('<img>', {
+    'src': imageLink
+  });
+  newImg.addClass("images");
+  // append it to the grid
+  $('#image-grid').append(newImg);
+}
+
+// get the form
+var marsForm = $('#mars-form');
+
+// do something on submit
+marsForm.submit(function (event) {
+  // prevent the default behaviour of the form
+  event.preventDefault();
+
+
+  // send an ajax request to fetch the list of images
+  $.ajax({
+    type: 'get',
+    url: 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos',
+    // data is query params for get request
+    data: {
+      sol: $('#sol').val(),
+      page: $('#page').val(),
+      api_key: 'w1JsPEER3yDBIO4YduLYttbpO7fxpNCvV8L39lKD'
+    },
+    // on receiving data successfully, empty the grid and add new images
+    success: function (responseData) {
+      console.log('response data:', responseData)
+      let photos = responseData.photos;
+      $('#image-grid').empty();
+      for (let i = 0; i < photos.length; i++) {
+        createImage(photos[i].img_src);
+      }
+
+
+    },
+    // in case of an error, handle here
+    error: function () {
+      console.log('some error');
+    }
+
+
+  });
+});
